fc7dc23ef6331b110e8f2119586a9d2e157c57dc
amagolden <amagoldenstein@gmail.com>
2024-10-09 11:17:23 -0500

    updated Firebase config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBt31tjVrlRqo_A5hGNI6SWxWDhLhKclY4",
-  authDomain: "r24-3-3be79.firebaseapp.com",
-  projectId: "r24-3-3be79",
-  storageBucket: "r24-3-3be79.appspot.com",
-  messagingSenderId: "209557629633",
-  appId: "1:209557629633:web:bd95ca81edd19d4b4e4240"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
